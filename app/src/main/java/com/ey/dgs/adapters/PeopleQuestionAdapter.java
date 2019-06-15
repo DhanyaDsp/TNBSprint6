d50182f2722aeb31c8c3919fc57cef333d56f223
Parvathy Geethakumari <Parvathy.Geethakumari@gds.ey.com>
2019-06-15 12:09:35 +0530

    PeopleQUestionAdapter

--- app/src/main/java/com/ey/dgs/adapters/PeopleQuestionAdapter.java
+++ app/src/main/java/com/ey/dgs/adapters/PeopleQuestionAdapter.java
@@ -1,7 +1,6 @@
 package com.ey.dgs.adapters;
 
 import android.support.annotation.NonNull;
-import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -16,6 +15,7 @@ import java.util.ArrayList;
 public class PeopleQuestionAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private int displayCount;
+    ArrayList<Integer> displayCounts = new ArrayList<>();
     ArrayList<String> nicknames;
     ArrayList<String> accountNumber;
 
@@ -32,10 +32,33 @@ public class PeopleQuestionAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
         PeopleQuestionHolder holder = (PeopleQuestionHolder) viewHolder;
-        holder.tvNickname.setText(nicknames.get(i));
-        holder.tvAccountNo.setText(accountNumber.get(i));
+        holder.tvNickname.setText(nicknames.get(position));
+        holder.tvAccountNo.setText(accountNumber.get(position));
+        displayCounts.add(position, 0);
+        holder.decrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(displayCounts.get(position) > 1) {
+                    int number = displayCounts.get(position);
+                     number= number - 1;
+                    holder.numberDisplay.setText("" + number);
+                    displayCounts.set(position, number);
+                }
+            }
+        });
+        holder.increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(displayCounts.get(position) <  10) {
+                    int number = displayCounts.get(position);
+                    number= number + 1;
+                    holder.numberDisplay.setText("" + number);
+                    displayCounts.set(position, number);
+                }
+            }
+        });
     }
 
     @Override
@@ -58,7 +81,7 @@ public class PeopleQuestionAdapter extends RecyclerView.Adapter<RecyclerView.Vie
         }
     }
 
-    public void increaseInteger() {
+    /*public void increaseInteger() {
         if(displayCount < 10) {
             displayCount = displayCount + 1;
             display(displayCount);
@@ -74,5 +97,5 @@ public class PeopleQuestionAdapter extends RecyclerView.Adapter<RecyclerView.Vie
 
     private void display(int number) {
         //numberDisplay.setText("" + number);
-    }
+    }*/
 }
